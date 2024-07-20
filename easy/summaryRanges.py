1c3d3d7ddb9e1ae73efab165071494cf1f3ada40
qqaazz800624 <qqaazz800624@gmail.com>
2024-07-20 12:32:41 +0800

    add No. 228

--- easy/summaryRanges.py
+++ easy/summaryRanges.py
@@ -1,28 +1,21 @@
 #%%
+from typing import List
 
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
 
+        start = 0
 
+        for i in range(len(nums)):
+            if i == len(nums) - 1 or nums[i] + 1 != nums[i+1]:
+                if start == i:
+                    result.append(str(nums[start]))
+                else:
+                    result.append(f"{nums[start]}->{nums[i]}")
+                start = i + 1
 
-
-#%%
-
-
-
-
-
-
-#%%
-
-
-
-
-
-
-#%%
-
-
-
-
+        return result
 
 
 #%%
