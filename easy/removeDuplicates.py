77ed075899a858ca0de15315f8ce335b2277d010
qqaazz800624 <qqaazz800624@gmail.com>
2024-08-03 15:31:52 +0800

    add No. 80

---
+++ easy/removeDuplicates.py
@@ -0,0 +1,26 @@
+#%%
+
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        write_index = 1
+        counter = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                counter += 1
+            else:
+                counter = 1
+            
+            if counter <= 2:
+                nums[write_index] = nums[i]
+                write_index += 1
+
+        return write_index
+
+
+#%%     
